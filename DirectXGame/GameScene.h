6f00d0debfed303d104a0c83011a5952093c22a7
SR0204 <k023g0023@g.neec.ac.jp>
2024-10-02 14:37:35 +0900

    ゲームシーンを作った

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,35 @@
+#pragma once
+
+	
+
+/// <summary>
+/// ゲームシーン
+/// </summary>
+class GameScene {
+
+public:
+
+	GameScene();
+
+	~GameScene();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	void Draw();
+
+
+private:
+
+	
+};
